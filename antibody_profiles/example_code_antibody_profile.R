aa395fec1fcdac46fd19c3693d049bf50d0a7c98
Adinda Kok <45686482+Adindakok@users.noreply.github.com>
2024-08-02 11:34:11 +0200

    remove working directory in example code

--- antibody_profiles/example_code_antibody_profile.R
+++ antibody_profiles/example_code_antibody_profile.R
@@ -5,13 +5,10 @@ library(Racmacs)
 library(r3js)
 #packages also needed: grDevices, viridis
 
-#set working directory
-setwd("~/Documents/R/202307_H5_manuscript_sera_landscapes/github_20240801/")
-
 #input code & data
 source("antibody_profile.R")
 h5_map <- read.acmap("input/H5_map.ace")
-maps_merged_whole_overlay <- readRDS("Output/maps_merged_whole_overlay_march24.Rdata")
+maps_merged_whole_overlay <- readRDS("output/maps_merged_whole_overlay_march24.Rdata")
 
 antibody_profile(map = h5_map, 
                  serum = 14, #the index number of the serum
