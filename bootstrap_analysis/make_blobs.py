2cf8798511e1fc01e65c0d56f811d21a11ef377b
Avicenna <sinatureli@gmail.com>
2025-05-06 12:22:26 +0100

    changed bayesian bootstrap parameters to default and fixed some problems that Adinda has identified

--- bootstrap_analysis/make_blobs.py
+++ bootstrap_analysis/make_blobs.py
@@ -14,8 +14,8 @@ import pandas as pd
 blobs_path = "./outputs/bootstrapped_map_bayesian.ace"
 bootstrapped_map = pr.read_racmap(blobs_path)
 
-map_with_blobs = pr.bootstrap_blobs(bootstrapped_map, conf=0.6,
-                                      grid_spacing=1, smoothing=6)
+map_with_blobs = pr.bootstrap_blobs(bootstrapped_map, conf=0.68,
+                                    grid_spacing=0.25, smoothing=8)
 
 ag_blobs, sr_blobs = map_with_blobs.get_blobs()
 
@@ -25,11 +25,9 @@ sr_vol, sr_maxsize = pr.analyse_blobs(sr_blobs)
 names = ["ag","sr"]
 
 for indt,vol_type in enumerate([ag_maxsize, sr_maxsize]):
-
   df = pd.DataFrame(list(vol_type.values()), index=vol_type, dtype=object)
   df.to_csv(f"./tables/maxsize_{names[indt]}.csv", index=True)
 
 for indt,vol_type in enumerate([ag_vol, sr_vol]):
-
-      df = pd.DataFrame(list(vol_type.values()), index=vol_type, dtype=object)
-      df.to_csv(f"./tables/vol_{names[indt]}.csv", index=True)
+  df = pd.DataFrame(list(vol_type.values()), index=vol_type, dtype=object)
+  df.to_csv(f"./tables/vol_{names[indt]}.csv", index=True)
